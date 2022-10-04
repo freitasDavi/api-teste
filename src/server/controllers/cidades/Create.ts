8ac5036d043f3dfbf2110b6f50909932eacceb2d
Davi <davi.frrs@gmail.com>
2022-10-04 16:12:47 -0300

    feat: Saving data in the database

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import * as yup from "yup";
 import { ICidade } from "../../database/models";
+import { CidadesProvider } from "../../database/providers/cidades";
 import { validation } from "../../shared/middlewares";
 
 interface IBodyProps extends Omit<ICidade, "id"> {}
@@ -9,11 +10,21 @@ interface IBodyProps extends Omit<ICidade, "id"> {}
 // Validação 
 export const createValidation = validation((getSchema) => ({
   body: getSchema<IBodyProps>(yup.object().shape({
-    nome: yup.string().required().min(3)
+    nome: yup.string().required().min(3).max(150)
   }))
 }));
 
 
 export const create = async (request: Request<{}, {}, ICidade>, response: Response) => {
-  return response.status(StatusCodes.CREATED).json(1);
+  const result = await CidadesProvider.create(request.body);
+
+  if (result instanceof Error) {
+    return response.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errors: {
+        default: result.message
+      }
+    });
+  }
+  
+  return response.status(StatusCodes.CREATED).json(result);
 };
