673111ab63c327e784b9e8d3958add63bebf0df9
Davi <davi.frrs@gmail.com>
2022-10-04 15:59:53 -0300

    feat: added cidades model and update methods

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,15 +1,14 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import * as yup from "yup";
+import { ICidade } from "../../database/models";
 import { validation } from "../../shared/middlewares";
 
-interface ICidade {
-  nome: string;
-}
+interface IBodyProps extends Omit<ICidade, "id"> {}
 
 // Validação 
 export const createValidation = validation((getSchema) => ({
-  body: getSchema<ICidade>(yup.object().shape({
+  body: getSchema<IBodyProps>(yup.object().shape({
     nome: yup.string().required().min(3)
   }))
 }));
