1d27767e8c53da5bdccc5d3fe809ba890c0bea61
Davi <davi.frrs@gmail.com>
2022-10-05 11:52:38 -0300

    feat: auto run migrations set

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
 import { server } from "./server/Server";
+import { Knex } from "./server/database/knex";
 
-server.listen(process.env.PORT || 3333, () => {
-  console.log(`Servidor rodando na porta ${process.env.PORT || 3333}`);
-});
+const startServer = () => {
+  server.listen(process.env.PORT || 3333, () => {
+    console.log(`App rodando na porta ${process.env.PORT}`);
+  });
+};
+
+if (process.env.IS_LOCALHOST !== "true") {
+  console.log("Rodando migrations");
+
+
+  Knex.migrate
+    .latest()
+    .then(() => startServer())
+    .catch(console.log);
+} else {
+  startServer();
+}
